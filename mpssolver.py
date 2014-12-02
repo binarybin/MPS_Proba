866e96deae582ee625af6cc6e60da43839947318
binarybin <binx@princeton.edu>
2014-12-01 23:24:55 -0500

    Job re-assigned, MPS and MPO defined

--- mpssolver.py
+++ mpssolver.py
@@ -4,15 +4,25 @@ Program purpose: The Alpha version of the APC 524 project.
 File name: mpssolver.py
 File purpose: the solver class based on the matrix product states
 Responsible persons: 
-    Liangsheng Zhang and Jun Xiong for Contraction and Compression
+    Peiqi Wang and Jun Xiong for Contraction and Compression
     Bin Xu for Interpreter
 """
 from solver import Solver
+from numpy import ndarray
 
 class MpsSolver(Solver):
     """
     The exact solver base class, using transition matrix method
     """
+    
+    mps_element = ndarray(shape = (2, 10, 10), dtype = float) # this is just an example of the mps, the order of indices: physical, left_aux, right_aux
+    mpo_element = ndarray(shape = (2, 2, 4, 4), dtype = float) # this is just an example of the mpo, the order of indices: physical_in, physical_out, left_aux, right_aux
+    
+    mps_chain = [] # list of mps
+    
+    mps_result = [] # list of mps_chain, result history
+    
+    
     def __init__(self, model, bound_dimension, output1, output2):
         self.model = model
         self.output1 = output1
@@ -24,8 +34,19 @@ class MpsSolver(Solver):
         raise NotImplementedError("please implement")
     def Evolve(self):
         raise NotImplementedError("please implement")
-        
+            
     def Contraction(self):
         raise NotImplementedError("please implement")
-    def Compression(self):
+    def CompressionSVD(self):
+        """
+        The compression based on SVD, to be implemented by Jun Xiong
+        """
         raise NotImplementedError("please implement")
+    def CompressionVariational(self):
+        """
+        The compression based on the variational principle, to be implemented by Peiqi Wang
+        """
+        raise NotImplementedError("please implement")
+    def Compression(self):
+        self.CompressionSVD()
+        self.CompressionVariational()

a2b6b8f19b05cb092dc1fab98492fbcc533916f9
Liangsheng <liangshe@princeton.edu>
2014-12-05 16:19:09 -0500

    Allow time not specified or negative. Allow user to specify up and down values

--- measurement.py
+++ measurement.py
@@ -20,16 +20,16 @@ class Measurement(object):
         self.output2 = output2
     def addMeasureTask(self, task):
         self.measurement_list.append(task)
-    
+
     def measure(self):
         for task in self.measurement_list:
             self.measure_result_list.append(eval("measure"+task[0]+"(task)"))
-    
-    
-    # All measure functions take the same input. They have different requested tasks and each measurement function should check the task properties. Raise an exception if 
+
+
+    # All measure functions take the same input. They have different requested tasks and each measurement function should check the task properties. Raise an exception if
     # it does not fit.
-        
-    # task is a tuple that shows up in the form (task_type_string, time_point, [points of interest]), 
+
+    # task is a tuple that shows up in the form (task_type_string, time_point, [points of interest]), if time_point is not given, then it is assumed to be the last time step in calculation. It can also be negative which then follows Python's convention.
     # for example, ("correlation", 1, [1, 2, 3, 4, 5]) measures <S1*S2*S3*S4*S5> at t = 1
     # task_type_string = "Correlation", "Mean", "Variance", "Proba", more to be added
     # some examples:
@@ -42,20 +42,20 @@ class Measurement(object):
         This implements the measurement of the probability (possibly joint probability) for an event or several events to be realized
         """
         raise NotImplementedError("Unimplemented abstract method")
-        
-    def measureCorrelation(self, task):
+
+    def measureCorrelation(self, task, up=None, down=None):
         """
         This implements the measurement of the n-point correlation function
         """
         raise NotImplementedError("Unimplemented abstract method")
-        
-    def measureMean(self, task):
+
+    def measureMean(self, task, up=None, down=None):
         """
         Mean is a special case of correlation, with only one variable. Just find the correct type and call measureCorrelation
         """
         raise NotImplementedError("Unimplemented abstract method")
-        
-    def measureVariance(self, task):
+
+    def measureVariance(self, task, up=None, down=None):
         """
         Mean is a special case of correlation, with only one variable. Just find the correct type and call measureCorrelation
         """

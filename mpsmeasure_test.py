6490884b8bdc75c26b5340f2cebf02869469cce4
Liangsheng <liangshe@princeton.edu>
2014-12-10 22:55:49 -0500

    Add tests of two sites for mpsmeasurement

--- mpsmeasure_test.py
+++ mpsmeasure_test.py
@@ -33,7 +33,7 @@ class MpsMeasurementTest(unittest.TestCase):
             corr = 1-2*p
 
             task_mean = ("Mean", [0])
-            mean = measure.measureCorrelation(task_mean)
+            mean = 1-2*p
 
             task_var = ("Variance", [0])
             var = 1 - (1-2*p)**2
@@ -48,6 +48,52 @@ class MpsMeasurementTest(unittest.TestCase):
 
             for i in xrange(len(result)):
                 self.assertAlmostEqual(measure.measure_result_list[i], result[i])
+
+    def testTwosties(self):
+        solver = TestSolver(2)
+        output1 = []
+        output2 = []
+
+        w1 = np.ndarray(shape = (2, 2), dtype = float)
+        w2 = np.ndarray(shape = (2, 2), dtype = float)
+
+        for p1 in np.linspace(0,1,10):
+            w1[0] = [p1/2, p1]
+            w1[1] =  [(1-p1)/2, 1-p1]
+
+            for p2 in np.linspace(0,1,10):
+                w2[0] = [p2, p2/2]
+                w2[1] =  [1-p2, (1-p2)/2]
+
+                solver.mps_result = [[],[w1,w2]]
+
+                measure = MpsMeasurement(solver, output1, output2)
+
+                task_up_up = ("Proba", [(0, "up"), (1, "up")])
+                prob_up_up = (1-p1) * (1-p2)
+
+                task_down_up = ("Proba", [(0, "down"), (1, "up")])
+                prob_down_up = p1 * (1-p2)
+
+                task_corr = ("Correlation", [0,1])
+                corr = (1-p1)*(1-p2) + p1*p2 - (1-p1)*p2 - (1-p2)*p1
+
+                task_mean = ("Mean", [0])
+                mean = 1 - 2*p1
+
+                task_var = ("Variance", [1])
+                var = 1 - (1-2*p2)**2
+
+                task = [task_up_up, task_down_up, task_corr, task_mean, task_var]
+                result = [prob_up_up, prob_down_up, corr, mean, var]
+
+                for n in task:
+                    measure.addMeasureTask(n)
+
+                measure.measure()
+
+                for i in xrange(len(result)):
+                    self.assertAlmostEqual(measure.measure_result_list[i], result[i])
 
 
 

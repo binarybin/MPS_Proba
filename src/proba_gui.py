cdaef032a8f6935e2d6cee506249f83f5a9d7b60
binarybin <binx@princeton.edu>
2015-01-11 21:08:13 -0500

    start working on the gui

---
+++ src/proba_gui.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+
+
+from matplotlib.figure import Figure
+
+import sys
+import Tkinter as Tk
+
+root = Tk.Tk()
+root.wm_title("Embedding in TK")
+
+
+f = Figure(figsize=(5,4), dpi=100)
+
+canvas = FigureCanvasTkAgg(f, master=root)
+canvas.show()
+canvas.get_tk_widget().grid(row=0, column=1)
+
+toolbar = NavigationToolbar2TkAgg( canvas, root )
+toolbar.update()
+canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
+def on_key_event(event):
+    print('you pressed %s'%event.key)
+    matplotlib.backend_bases.key_press_handler(event, canvas, toolbar)
+
+canvas.mpl_connect('key_press_event', on_key_event)
+
+def _quit():
+    root.quit()     # stops mainloop
+    root.destroy()  # this is necessary on Windows to prevent
+                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+button = Tk.Button(master=root, text='Quit', command=_quit)
+button.pack(side=Tk.BOTTOM)
+
+Tk.mainloop()
+# If you put root.destroy() here, it will cause an error if
+# the window is closed with the window manager.
+
+
+
